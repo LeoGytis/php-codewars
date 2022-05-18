fad8cd7c604457d9e39edf857aca553f2a248ed4
LeoGytis <leogytis@gmail.com>
2022-05-18 10:33:29 +0300

    Nezinau kodel

--- youre-square.php
+++ youre-square.php
@@ -5,16 +5,23 @@ function isSquare($n) {
     $m = sqrt($n);
     echo $m;
     if ($n < 2 || !is_int($m)) {
-        echo 'false yra false';
+        echo 'false yra false <br>';
         return false;
-    } elseif (is_int($m)) {
-        echo 'true yra true';
+    } else {
+        echo 'true yra true <br>';
         return true; 
     }  
 }
 
+function arInt($n) {
+    if (is_int($n)) {
+        echo "NE INT";
+    } else echo 'Integer';
+}
 
+// arInt(3.5553);
 // echo is_int(3.5553);
+
 isSquare(5);
 isSquare(9);
 // echo isSquare(2922221507690916025);
